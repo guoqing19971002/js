81f11e27703d81b676d0da3e707baa5d91ea832e
31536879@qq.com <31536879@qq.com>
2020-09-19 11:24:48 +0800

    update

--- js事件循环和任务队列.js
+++ js事件循环和任务队列.js
@@ -48,7 +48,7 @@ setTimeout(() => {
         console.log('3') 
         resolve()
     }).then((res) =>{
-            console.log('4') // 代码执行到这里，微任务只有一个，打印4，继续执行，.then微任务入微任务队列，则继续执行，打印5.之后微任务队列无新任务
+            console.log('4') // 代码执行到这里，微任务只有一个，打印4，继续执行，.then进入微任务队列，则继续执行，打印5.之后微任务队列无新任务
             //则继续执行宏任务队列
             new Promise(resolve => { 
                 resolve()
