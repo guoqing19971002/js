2fa91d9e2e51805f2b5034f0f84c77d540f454d6
郭庆 <31536879@qq.com>
2021-03-28 16:26:43 +0800

    async

---
+++ async.js
@@ -0,0 +1,45 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-28 16:21:39
+ * @LastEditTime: 2021-03-28 16:26:01
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \js\async.js
+ */
+async function async1 () {
+    console.log('async1 start')
+    await async2()
+    console.log('async1 end') // 关键在这一步，它相当于放在 callback 中，最后执行
+    await async3()
+    console.log('async1 end2')
+  }
+  
+  async function async2 () {
+    console.log('async2')
+  }
+  async function async3 () {
+    console.log('async3')
+  }
+  console.log('script start');
+  (async function(){
+    await async1()
+    console.log('script end');
+  })()
+ 
+  /*
+   script start
+   async1 start
+   async2
+   script end
+   async1 end
+   async3
+   async1 end2
+
+   script start
+   async1 start
+   async2
+   async1 end
+   async3
+   async1 end2
+   script end
+   */
