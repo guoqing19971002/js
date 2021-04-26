4574c1c2708bb6beebcd4a09433b68182b0d6812
郭庆 <31536879@qq.com>
2021-04-26 13:38:08 +0800

    add

--- type/isType.js
+++ type/isType.js
@@ -1,17 +1,9 @@
 // 这是一个通用的类型判断方法
 function ifType(val) {
-  let type = typeof val;
-  if (type !== "object") {
-    // 先进行typeof判断，如果是基础数据类型，直接返回
-    return type;
-  }
-  // 对于typeof返回结果是object的，再进行如下的判断，正则返回结果
   return Object.prototype.toString
     .call(val)
     .replace(/^\[object (\S+)\]$/, "$1");
 }
-
-
 console.log(ifType(1))
 console.log(ifType('2'))
 console.log(ifType({name:'123'}))
