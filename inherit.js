3c3905a218e0630bf9c90ad569ee350695aa6635
郭庆 <31536879@qq.com>
2021-04-26 17:15:17 +0800

    add

--- inherit.js
+++ inherit.js
@@ -39,5 +39,28 @@ var child2 = new Child();
 child1.colors.push('yellow')
 console.log(child2.colors) */
 
-
-// 
+//
+function Father(name, age) {
+  this.name = name;
+  this.age = age;
+  this.run = function () {
+    console.log("我会跑步");
+  };
+}
+function Son(name, age) {
+  Father.call(this, name, age);
+}
+/*
+可以使用Object.create方法创建一个父类原型的副本,其余操作与组合继承一致。
+只调用了一次SuperType构造函数
+ */
+let protoType = Object.create(Father);
+Son.protoType = protoType;
+protoType.constructor = Son;
+Son.prototype.say = function() { // 这是子类的独有方法
+    console.log('我是小孩');
+}
+const p1 = new Son("李四", 10);
+console.log(p1.name);
+p1.run(); // 我会跑步
+p1.say() // 我是小孩
